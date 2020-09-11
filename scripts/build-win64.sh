bbecf61c317dc2be23f49c162232dd33cf1447f2
TFinch <travisfinch01@gmail.com>
2020-09-10 23:21:35 -0400

    correct -1 to -y

--- scripts/build-win64.sh
+++ scripts/build-win64.sh
@@ -15,7 +15,7 @@ sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev open
 sudo apt-get install libssl1.0-dev -y
 
 sudo apt install g++-mingw-w64-x86-64 -y
-sudo update-alternatives --config x86_64-w64-mingw32-g++ -1 # Set the default mingw32 g++ compiler option to posix. or choose 0 for deb8
+sudo update-alternatives --config x86_64-w64-mingw32-g++ -y # Set the default mingw32 g++ compiler option to posix. or choose 0 for deb8
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
 cd depends
 sudo make HOST=x86_64-w64-mingw32 -j4
